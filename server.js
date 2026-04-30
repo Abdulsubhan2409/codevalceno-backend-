be6a5c0216ed904cd5d4f423b0bb45b92c1fc6d6
Abdul Subhan <abdulsubhanabbasi456@gmail.com>
2026-04-30 15:27:27 +0500

    fix: remove dotenv - Railway injects env vars automatically

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const cors = require("cors");
 const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
 const nodemailer = require("nodemailer");
-require("dotenv").config();
+
 
 const app = express();
 
